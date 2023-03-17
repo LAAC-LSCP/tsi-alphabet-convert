c30439fe772bd386791f6a4a47fc47f5b83b4e75
William N. Havard <william.havard@gmail.com>
2023-03-17 17:28:37 +0100

    Added example

--- language.js
+++ language.js
@@ -16,7 +16,12 @@ var es_BO = {
     bajo la supervisión de Alejandrina Cristia, directora de investigación.<br/>\
     Equipo LAAC-LSCP en la ENS de París,<br/>\
     29 rue d'Ulm 75005 París, Francia",
-    legal: "Aviso legal y datos personales"
+    legal: "Aviso legal y datos personales",
+	step_one: "Al empezar, el sitio web tiene este aspecto.",
+	step_two: "Escriba o copie/pegue una frase escrita en el alfabeto antiguo y haga clic en \"Antiguo\" en la parte superior izquierda. El botón \"Antiguo\" aparecerá ahora en rojo. Aquí utilizo la frase \"Yu ra' yi mo' p̂eyacdye' pa'tsaqui.\" como ejemplo. Pero puedes escribir o copiar/pegar cualquier texto, ¡no importa lo largo que sea!",
+	step_three: "Haga clic en \"Nuevo\" para convertir el texto en el nuevo alfabeto. \"Nuevo\" aparecerá en verde. Inmediatamente, el texto convertido aparecerá debajo, delante de la frase del alfabeto antiguo. Aquí la frase con el antiguo alfabeto \"Yu ra' yi mo' p̂eyacdye' pa'tsaqui.\" se convirtió en \"Yu ra' yi mo' pheyakdye' pa'tsaki.\" en el nuevo alfabeto.",
+	step_four: "Si quieres, también puedes convertir una frase escrita en el tipo de letra \"Times Chimani\" como se muestra aquí \"Yu ra' yi mo' ƒeyacdye' pa'tsaqui\". Aquí elegí convertirla al nuevo alfabeto (fíjate que \"Nuevo\" aparece en verde) y aparece el texto convertido: \"Yu ra' yi mo' pheyakdye' pa'tsaki.\"",
+	summary: "Ver un ejemplo paso a paso",
 }
 
 var en_US = {
@@ -37,7 +42,12 @@ var en_US = {
     under the supervision of Alejandrina Cristia, research director.<br/>\
     LAAC-LSCP team at ENS Paris,<br/>\
     29 rue d'Ulm 75005 Paris, France",
-    legal: "Legal notice and personal data"
+    legal: "Legal notice and personal data",
+	step_one: "When you start, the website looks like this.",
+	step_two: "Type or copy/paste a phrase written in the old alphabet and click on \"Old\" in the top left. The \"Old\" button will now appear in red. Here I use the phrase \"Yu ra' yi mo' p̂eyacdye' pa'tsaqui\" as an example. But you can type or copy/paste any text, no matter how long it is!",
+	step_three: "Click \"New\" to convert the text to the new alphabet. \"New\" will appear in green. Immediately, the converted text will appear below, in front of the old alphabet sentence. Here the old alphabet sentence \"Yu ra' yi mo' p̂eyacdye' pa'tsaqui\" has been converted to \"Yu ra' yi mo' pheyakdye' pa'tsaki.\"",
+	step_four: "If you want, you can also convert a sentence written in the font \"Times Chimani\" as shown here \"Yu ra' yi mo' ƒeyacdye' pa'tsaqui\". Here I chose to convert it to the new alphabet (note that \"New\" is shown in green) and the converted text appears: \"Yu ra' yi mo' pheyakdye' pa'tsaki.\"",
+	summary: "See a step-by-step example",
 }
 
 var fr_FR = {
@@ -49,8 +59,8 @@ var fr_FR = {
     Vous devez simplement copier et coller le text dans l'emplacement à gauche, sélectionner l'alphabet de départ and celui voulu en sortie, le texte converti apparait alors dans l'emplacement à droite.\
     Les boutons rayés ne peuvent être pressés.",
     chimani: "Chimani",
-    old: "ancien",
-    new: "nouveau",
+    old: "Ancien",
+    new: "Nouveau",
     left_placeholder: "Écrire le texte de départ ici...",
     right_placeholder: "et le texte converti est affiché ici!",
     credits: "Crédits:<br/>\
@@ -58,7 +68,12 @@ var fr_FR = {
     sous la supervision de Alejandrina Cristia, directrice de recherche.<br/>\
     équipe LAAC-LSCP à ENS Paris,<br/>\
     29 rue d'Ulm 75005 Paris, France",
-    legal: "Mentions légales et données personnelles"
+    legal: "Mentions légales et données personnelles",
+	step_one : "Lorsque vous commencez, le site web ressemble à ceci.",
+	step_two : "Tapez ou copiez/collez une phrase écrite dans l'ancien alphabet et cliquez sur \"Ancien\" en haut à gauche. Le bouton \"Ancien\" apparaît alors en rouge. J'utilise ici la phrase \"Yu ra' yi mo' p̂eyacdye' pa'tsaqui\" comme exemple. Mais vous pouvez taper ou copier/coller n'importe quel texte, quelle que soit sa longueur !",
+	step_three : "Cliquez sur \"Nouveau\" pour convertir le texte dans le nouvel alphabet. \"Nouveau\" apparaît en vert. Le texte converti apparaît immédiatement en dessous, devant l'ancienne phrase alphabétique. Ici, l'ancienne phrase \"Yu ra' yi mo' p̂eyacdye' pa'tsaqui\" a été convertie en \"Yu ra' yi mo' pheyakdye' pa'tsaki.\"",
+	step_four : "Si vous le souhaitez, vous pouvez également convertir une phrase écrite dans la police \"Times Chimani\" comme ici \"Yu ra' yi mo' ƒeyacdye' pa'tsaqui\". Ici, j'ai choisi de la convertir au nouvel alphabet (notez que \"New\" est affiché en vert) et le texte converti apparaît : \"Yu ra' yi mo' pheyakdye' pa'tsaki.\"",
+	summary: "Voir un exemple pas-à-pas",
 }
 
 function set_lang(lang) {
@@ -81,6 +96,12 @@ function set_lang(lang) {
 
     document.getElementById("credits").innerHTML = lang.credits
     document.getElementById("legal").innerHTML = lang.legal
+	
+	document.getElementById("step_one").innerHTML = lang.step_one
+	document.getElementById("step_two").innerHTML = lang.step_two
+	document.getElementById("step_three").innerHTML = lang.step_three
+	document.getElementById("step_four").innerHTML = lang.step_four
+	document.getElementById("summary").innerHTML = lang.summary
 
     Array.prototype.forEach.call(document.getElementsByClassName("button-lang"), function(element) {
         if (element.id == lang.id) {
